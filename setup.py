c4d37343ab78c8d8e7cbec96f5df2e0b306eeb5c
Howard <makubex49@gmail.com>
2025-02-06 15:57:19 +0800

    colcon build 時會自動將.ui檔轉換成py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 from setuptools import find_packages, setup
+
+import os
 import glob
+import subprocess
 
 package_name = 'ros2_pyqt_demo'
+
+# -----------------------------------------------------
+# 自動轉換 .ui
+# -----------------------------------------------------
+def convert_ui():
+    ui_files = glob.glob("ros2_pyqt_demo/*.ui")  
+    output_dir = "ros2_pyqt_demo"
+    os.makedirs(output_dir, exist_ok=True)
+    for ui_file in ui_files:
+        py_file = os.path.join(output_dir, f"ui_{os.path.basename(ui_file).replace('.ui', '.py')}")
+        subprocess.run(["pyside6-uic", "-o", py_file, ui_file], check=True)
+# -----------------------------------------------------
+convert_ui()
+# -----------------------------------------------------
 
 setup(
     name=package_name,
